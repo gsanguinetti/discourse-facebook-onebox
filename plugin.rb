1a8fff7bead762d0d2be4584a868ecd88fae1118
Huan Nghiem <nightshade.bld@gmail.com>
2016-12-24 17:42:13 +0700

    test accept header 'vi'

--- plugin.rb
+++ plugin.rb
@@ -29,7 +29,39 @@ module Onebox
       end
 
       private
+			def fetch_response(location, limit = 5, domain = nil, headers = nil)
+				raise Net::HTTPError.new('HTTP redirect too deep', location) if limit == 0
 
+				uri = URI(location)
+				if !uri.host
+					uri = URI("#{domain}#{location}")
+				end
+				http = Net::HTTP.new(uri.host, uri.port)
+				http.open_timeout = Onebox.options.connect_timeout
+				http.read_timeout = Onebox.options.timeout
+				if uri.is_a?(URI::HTTPS)
+					http.use_ssl = true
+					http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+				end
+
+				headers['accept-language'] = 'vi' if headers.is_a? Hash
+  			headers = {'accept-language' => 'vi'} if headers == nil
+
+				response = http.request_get(uri.request_uri,headers)
+
+				cookie = response.get_fields('set-cookie')
+				if (cookie)
+					header = {'cookie' => cookie.join("")}
+				end
+				header = nil unless header.is_a? Hash
+
+				case response
+					when Net::HTTPSuccess     then response
+					when Net::HTTPRedirection then fetch_response(response['location'], limit - 1, "#{uri.scheme}://#{uri.host}",header)
+					else
+						response.error!
+				end
+			end
       def video?
         url =~ /\/video.php\?|\/videos\//
       end
@@ -46,9 +78,9 @@ module Onebox
 
       def get_oembed_data
         if video?
-          Onebox::Helpers.symbolize_keys(::MultiJson.load(Onebox::Helpers.fetch_response("https://facebook.com/plugins/video/oembed.json?url=#{url}&maxwidth=661&omitscript=true").body))
+          Onebox::Helpers.symbolize_keys(::MultiJson.load(fetch_response("https://facebook.com/plugins/video/oembed.json?url=#{url}&maxwidth=661&omitscript=true").body))
         else
-          Onebox::Helpers.symbolize_keys(::MultiJson.load(Onebox::Helpers.fetch_response("https://facebook.com/plugins/post/oembed.json?url=#{url}&maxwidth=661&omitscript=true").body))
+          Onebox::Helpers.symbolize_keys(::MultiJson.load(fetch_response("https://facebook.com/plugins/post/oembed.json?url=#{url}&maxwidth=661&omitscript=true").body))
         end
       end
     end
